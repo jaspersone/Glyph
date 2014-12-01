78991d40bba73752eb20a150ea72054eb588d3fd
jaspersone <jaspersone@gmail.com>
2014-12-01 00:46:30 -0800

    Adding base class to host logic for mapping a set of vertex pairs to a unique integer, which represents the state of the board

---
+++ nine_grid_mapper.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import sys
+import os
+
+
+class NineGridMapper(object):
+    def NineGridMapper():
+        ''' Setups up a mapping of key values to our internal
+            numbering system as shown in the following grid.
+            
+            The grid is a nine dotted grid, where the verticies
+            are label as numbers from 1 - 9:
+            
+            1 2 3
+            4 5 6
+            7 8 9
+            
+            The below shows potential edges that can be formed
+            between verticies. Please note the basic rules:
+                1. There exist no edge that connects a vertex
+                   to itself
+                2. There exist no edge that connects more than
+                   two verticies at one time
+                3. We describe edges as two number the start and
+                   the end of the line, where the start number
+                   is strictly less than the ending number.
+                4. x's represent keys that are not valid
+            
+            Table of Indexes based upon vertex pairs
+                1   2   3   4   5   6   7   8   9
+               ___________________________________
+            1 | x   0   x   1   2   3   x   4   x
+              |     
+            2 | x   x   5   6   7   8   9   x   10
+              |         
+            3 | x   x   x   11  12  13  x   14  x
+              |
+            4 | x   x   x   x   15  x   16  17  18
+              |
+            5 | x   x   x   x   x   19  20  21  22
+              |
+            6 | x   x   x   x   x   x   23  24  25
+              |
+            7 | x   x   x   x   x   x   x   26  x
+              |
+            8 | x   x   x   x   x   x   x   x   27
+              |
+            9 | x   x   x   x   x   x   x   x   x
+        '''
+        self.ninegrid = [
+                        (1,2), (1,4), (1,5), (1,6), (1,8),
+                        (2,3), (2,4), (2,5), (2,6), (2,7), (2,9),
+                        (3,4), (3,5), (3,6), (3,8),
+                        (4,5), (4,7), (4,8), (4,9),
+                        (5,6), (5,7), (5,8), (5,9),
+                        (6,7), (6,8), (6,9),
+                        (7,8),
+                        (8,9),
+                        ]
+
+    def encode_hash(pairs):
+        ''' A basic function that encodes a list of vertex pairs
+            into a unique hash.
+            
+            The basic algorithm is takes a given list of vertices (keys)
+            and assigns each pair a power of two. Since vertices must be
+            unqiue.
+        '''
+        # ensures no duplicate vertex pairs
+        keys = list(set(pairs))
+        hashed_result = 0
+        try:
+            for key in keys:
+                hashed_result += 2 ** self.ninegrid.index(key)
+            return hashed_result
+        except Exception as e:
+            return None
+    
+    def decode_hash(hashed_key):
+        ''' returns an list of edges described as (V1,V2)'''
+        if type(hashed_key) is not int:
+            return None
+        # create binary version of number (prefilled to a length of 28 chars)
+        binary = bin(hashed_key)[2:].zfill(28)
+        last_index = len(binary) - 1
+        result = []
+        for x in xrange(len(binary)):
+            if binary[last_index - x] is '1':
+                results.append(self.ninegrid[x])
+        return result
+    
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
