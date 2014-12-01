b6c697119370b3763713efcf470e4ba289ec0bbc
jaspersone <jaspersone@gmail.com>
2014-12-01 01:22:16 -0800

    Consolidating documentation above the class (so the class is more readable)

--- nine_grid_mapper.py
+++ nine_grid_mapper.py
@@ -4,50 +4,51 @@ import sys
 import os
 import pprint
 
+''' Setups up a mapping of key values to our internal
+    numbering system as shown in the following grid.
+    
+    The grid is a nine dotted grid, where the verticies
+    are label as numbers from 1 - 9:
+    
+    1 2 3
+    4 5 6
+    7 8 9
+    
+    The below shows potential edges that can be formed
+    between verticies. Please note the basic rules:
+        1. There exist no edge that connects a vertex
+           to itself
+        2. There exist no edge that connects more than
+           two verticies at one time
+        3. We describe edges as two number the start and
+           the end of the line, where the start number
+           is strictly less than the ending number.
+        4. x's represent keys that are not valid
+    
+    Table of Indexes based upon vertex pairs
+        1   2   3   4   5   6   7   8   9
+       ___________________________________
+    1 | x   0   x   1   2   3   x   4   x
+      |     
+    2 | x   x   5   6   7   8   9   x   10
+      |         
+    3 | x   x   x   11  12  13  x   14  x
+      |
+    4 | x   x   x   x   15  x   16  17  18
+      |
+    5 | x   x   x   x   x   19  20  21  22
+      |
+    6 | x   x   x   x   x   x   23  24  25
+      |
+    7 | x   x   x   x   x   x   x   26  x
+      |
+    8 | x   x   x   x   x   x   x   x   27
+      |
+    9 | x   x   x   x   x   x   x   x   x
+'''
+
 class NineGridMapper:
     def __init__(self):
-        ''' Setups up a mapping of key values to our internal
-            numbering system as shown in the following grid.
-            
-            The grid is a nine dotted grid, where the verticies
-            are label as numbers from 1 - 9:
-            
-            1 2 3
-            4 5 6
-            7 8 9
-            
-            The below shows potential edges that can be formed
-            between verticies. Please note the basic rules:
-                1. There exist no edge that connects a vertex
-                   to itself
-                2. There exist no edge that connects more than
-                   two verticies at one time
-                3. We describe edges as two number the start and
-                   the end of the line, where the start number
-                   is strictly less than the ending number.
-                4. x's represent keys that are not valid
-            
-            Table of Indexes based upon vertex pairs
-                1   2   3   4   5   6   7   8   9
-               ___________________________________
-            1 | x   0   x   1   2   3   x   4   x
-              |     
-            2 | x   x   5   6   7   8   9   x   10
-              |         
-            3 | x   x   x   11  12  13  x   14  x
-              |
-            4 | x   x   x   x   15  x   16  17  18
-              |
-            5 | x   x   x   x   x   19  20  21  22
-              |
-            6 | x   x   x   x   x   x   23  24  25
-              |
-            7 | x   x   x   x   x   x   x   26  x
-              |
-            8 | x   x   x   x   x   x   x   x   27
-              |
-            9 | x   x   x   x   x   x   x   x   x
-        '''
         # max_hash = 2^0 + 2^1 + 2^2 + ... 2^27
         self.max_hash = 268435455
         self.grid = [
@@ -73,18 +74,15 @@ class NineGridMapper:
         '''
         # ensures no duplicate vertex pairs
         keys = list(set(pairs))
-        hashed_result = 0
-        for key in keys:
-            try:
-                hashed_result += 2 ** self.grid.index(key)
-            except Exception as e:
-                ''' if an exception is thrown, it is likely due to the fact
-                    that a key of vertex pairs that was passed to encode_hash
-                    does not exist inside of self.grid.
-                '''
-                print e
-                return None
-        return hashed_result
+        try:
+            return sum([2 ** self.grid.index(key) for key in keys])
+        except Exception as e:
+            ''' if an exception is thrown, it is likely due to the fact
+                that a key of vertex pairs that was passed to encode_hash
+                does not exist inside of self.grid.
+            '''
+            print e
+            return None
     
     def decode_hash(self, hashed_key):
         ''' params: hashed_key is an integer between 0 and self.max_hash
